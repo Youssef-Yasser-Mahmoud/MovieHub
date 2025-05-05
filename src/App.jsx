dc69602d653bf215b04029f079e70060932ef284
Youssef <youssefyaser2020@gmail.com>
2025-05-05 23:02:51 +0300

    Setup React-Router for the web

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import NavBar from './components/NavBar';
+
 function App() {
   return (
     <div>
-      hi
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='home' element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+function NotFound() {
+  return (
+    <div>
+      err
+    </div>
+  )
+}
+
+export default NotFound
